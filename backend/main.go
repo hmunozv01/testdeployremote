b5cbbe89c99f6b80d317447abee7bce34121a14b
hmunozv01 <42751145+hmunozv01@users.noreply.github.com>
2022-01-05 13:09:45 -0300

    cambio backend

--- backend/main.go
+++ backend/main.go
@@ -22,7 +22,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 Hello from Docker!
 ESTE ES MI AMBIENTE DEVELOP ahora si que si
 
-Cambio en repo develop para despliegue local.
+CAMBIO REALIZADO EN REUNION
 
 `)
 }
