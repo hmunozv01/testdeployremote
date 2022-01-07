5062f581006dd47012c83b440c7e989db6a1993b
hmunozv01 <42751145+hmunozv01@users.noreply.github.com>
2022-01-07 10:25:26 -0300

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -22,8 +22,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 Hello from Docker!
 ESTE ES MI AMBIENTE DEVELOP ahora si que si
 
-CAMBIO REALIZADO EN REUNION
-
+cambio para pull request
 `)
 }
 
